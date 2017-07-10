61642b5c4ad397300924815db1f63311cfe0206a
mengguiyouziyi <775618369@qq.com>
2017-07-10 19:51:42 +0800

    1 version

--- zy_spy_170709/zy_spy_170709/settings.py
+++ zy_spy_170709/zy_spy_170709/settings.py
@@ -47,7 +47,7 @@ RETRY_HTTP_CODES = [500, 502, 503, 504, 400, 403, 408, 429]
 DOWNLOAD_TIMEOUT = 60
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
