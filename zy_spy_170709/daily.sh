ce98f550e3e0aca89ae23a1b9d83ed6d160f3ac5
mengguiyouziyi <775618369@qq.com>
2017-09-11 15:31:06 +0800

    daily

--- zy_spy_170709/daily.sh
+++ zy_spy_170709/daily.sh
@@ -10,15 +10,15 @@ eval "$(pyenv init -)"
 #/data1/spider/.pyenv/libexec/pyenv activate env_ana3-4.3.0
 pyenv activate env_ana3-4.3.0
 
-U_V1=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
-U_V2=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $2}'`
-U_V3=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $3}'`
-echo your python version is : $U_V1.$U_V2.$U_V3 >> /data1/spider/menggui/zhiyou/zy_spy_170709/111.out
+#U_V1=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $1}'`
+#U_V2=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $2}'`
+#U_V3=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $3}'`
+#echo your python version is : $U_V1.$U_V2.$U_V3 >> /data1/spider/menggui/zhiyou/zy_spy_170709/111.out
 
 cd /data1/spider/menggui/zhiyou/zy_spy_170709/
 
 nohup python /data1/spider/menggui/zhiyou/zy_spy_170709/zy_spy_170709/utils/send.py >>/data1/spider/menggui/zhiyou/zy_spy_170709/zy_spy_170709/utils/send.out 2>&1 &
-echo "bbb" >> /data1/spider/menggui/zhiyou/zy_spy_170709/111.out
+#echo "bbb" >> /data1/spider/menggui/zhiyou/zy_spy_170709/111.out
 
 sleep 1m
 nohup python /data1/spider/menggui/zhiyou/zy_spy_170709/cmd.py >>/data1/spider/menggui/zhiyou/zy_spy_170709/daily_1.out 2>&1 &
