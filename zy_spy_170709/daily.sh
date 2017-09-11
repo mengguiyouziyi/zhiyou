5e5a38bd90a6462f90b16c8c3680c39d663ee894
mengguiyouziyi <775618369@qq.com>
2017-09-11 14:17:23 +0800

    daily

--- zy_spy_170709/daily.sh
+++ zy_spy_170709/daily.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+sudo su - << EOF
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
 pyenv activate env_ana3-4.3.0
@@ -17,3 +17,4 @@ echo "33333" >> /data1/spider/menggui/zhiyou/zy_spy_170709/111.out
 sleep 1m
 nohup python /data1/spider/menggui/zhiyou/zy_spy_170709/cmd.py >>/data1/spider/menggui/zhiyou/zy_spy_170709/daily_1.out 2>&1 &
 nohup python /data1/spider/menggui/zhiyou/zy_spy_170709/cmd.py >>/data1/spider/menggui/zhiyou/zy_spy_170709/daily_2.out 2>&1 &
+EOF
