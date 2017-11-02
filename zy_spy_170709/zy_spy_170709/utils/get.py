a0c2f21333b7e15ff5186627af9f8835203860aa
mengguiyouziyi <775618369@qq.com>
2017-11-02 18:36:54 +0800

    daily

--- zy_spy_170709/zy_spy_170709/utils/get.py
+++ zy_spy_170709/zy_spy_170709/utils/get.py
@@ -1,44 +1,44 @@
-# coding:utf-8
-
-import os
-import sys
-from os.path import dirname
-
-from .my_redis import QueueRedis
-
-
-father_path = os.path.abspath(dirname(__file__))
-sys.path.append(father_path)
-
-
-red = QueueRedis()
-
-
-def get_key(key):
-	results = red.read_from_queue(key, 1)
-	if results:
-		result = results[0].decode().strip()
-		return result
-	else:
-		return 0
-
-
-
-
-
-
-
-
-# """
-# 本机 localhost；服务器 a027.hb2.innotree.org
-# """
+# # coding:utf-8
+#
+# import os
+# import sys
+# from os.path import dirname
+#
+# from .my_redis import QueueRedis
+#
+#
+# father_path = os.path.abspath(dirname(__file__))
+# sys.path.append(father_path)
+#
+#
 # red = QueueRedis()
 #
 #
-# def get_id():
-# 	results = red.read_from_queue('ids', 1)
+# def get_key(key):
+# 	results = red.read_from_queue(key, 1)
 # 	if results:
-# 		result = int(results[0])
+# 		result = results[0].decode().strip()
 # 		return result
 # 	else:
 # 		return 0
+#
+#
+#
+#
+#
+#
+#
+#
+# # """
+# # 本机 localhost；服务器 a027.hb2.innotree.org
+# # """
+# # red = QueueRedis()
+# #
+# #
+# # def get_id():
+# # 	results = red.read_from_queue('ids', 1)
+# # 	if results:
+# # 		result = int(results[0])
+# # 		return result
+# # 	else:
+# # 		return 0
